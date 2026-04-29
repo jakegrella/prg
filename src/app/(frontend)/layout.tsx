c5546026fe453d472f360ab542d63b3570ff64bc
Jake Grella <jake@jakegrella.com>
2026-04-28 19:51:52 -0700

    chore: add basic open graph info

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import type { Metadata } from 'next'
 import { JetBrains_Mono } from 'next/font/google'
 import './globals.css'
 
@@ -7,9 +8,26 @@ const jbm = JetBrains_Mono({
   variable: '--font-jbm',
 })
 
-export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+const siteDescription =
+  'Pappas Restaurant Group prides itself on providing an exceptional dining experience, combining exquisite cuisine with unparalleled service in a warm and inviting atmosphere.'
+
+export const metadata: Metadata = {
+  // metadataBase: new URL(process.env.NEXT_PUBLIC_SERVER_URL ?? 'http://localhost:3000'),
+  title: 'Pappas Restaurant Group',
+  description: siteDescription,
+  openGraph: {
+    title: 'Pappas Restaurant Group',
+    description: siteDescription,
+    url: '/',
+    siteName: 'Pappas Restaurant Group',
+    type: 'website',
+    locale: 'en_US',
+  },
+  twitter: {
+    card: 'summary',
+    title: 'Pappas Restaurant Group',
+    description: siteDescription,
+  },
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
